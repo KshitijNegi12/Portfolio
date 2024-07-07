522a0254f00e47c87621a59111a230feb8ef14bf
KshitijNegi12 <23kshitij.negi@gmail.com>
2024-07-07 15:56:44 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function resumedownload(){
+  const pdfUrl = './assets/resume.pdf';
+  const anchorElement = document.createElement('a');
+  anchorElement.href = pdfUrl;
+  anchorElement.download = 'Kshitij_Resume.pdf';
+  // document.body.appendChild(anchorElement);
+  anchorElement.click();
+  // document.body.removeChild(anchorElement);
+}
+
+function btnshadow(index, docs){
+  var content=document.querySelectorAll('.bar-btn')
+  for (let i = 0; i < content.length; i++) {
+    content[i].style.removeProperty('background-color');
+    content[i].style.removeProperty('box-shadow');
+  }
+  content[index].style.backgroundColor='#2492FF';
+  content[index].style.boxShadow='5px 5px rgb(48, 44, 44)';
+  var iframe=document.getElementById('myframe');
+  iframe.src=docs;
+}
